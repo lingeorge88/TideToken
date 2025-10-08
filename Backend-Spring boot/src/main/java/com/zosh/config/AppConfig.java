e20c93f040a8dea8f76b38e91f978db41b7d065e
George Lin <lingeorge04@gmail.com>
2025-10-08 14:25:52 -0700

    updated backend config to use new url

--- Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
+++ Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
@@ -90,7 +90,7 @@ public class AppConfig {
 	                    "http://localhost:5173",
 	                    "http://localhost:5174",
 	                    "http://localhost:4200",
-	                    "https://tidetoken.onrender.com"
+	                    "https://bluecurrent-zy4p.onrender.com"
 	                ));
 	                cfg.setAllowedMethods(Collections.singletonList("*"));
 	                cfg.setAllowCredentials(true);
